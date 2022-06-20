01158fcedf867ac172eda82e145aa59c1e3427ae
Michael <michaell10919@gmail.com>
2022-06-20 11:01:25 -0700

    Express Generator REST API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+// router.use("/campsites", require("./campsiteRouter"));
+// router.use("/promotions", require("./promotionRouter"));
+// router.use("/partners", require("./partnerRouter"));
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
